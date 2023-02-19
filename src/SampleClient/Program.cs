acdcea5f216076a75412d44fecf42cd4984c6524
Thomas Pettersen / Yrki <thomas@yrki.no>
2023-02-19 11:45:01 +0100

    Initial commit.

--- src/SampleClient/Program.cs
+++ src/SampleClient/Program.cs
@@ -1,11 +1,11 @@
-﻿using Yrki.IoT.WMBus.WurthMetis;
+﻿using Yrki.IoT.WMBus.IMSTim871A;
 
 // Set up the port and baudrate
-var port = "COM3"; // The port to use.
-var baudRate = 9600; // The baudrate to use.
+var port = "COM6"; // The port to use.
+var baudRate = 576; // The baudrate to use.
 
 // Create a new instance
-var metisII = new MetisII();
+var metisII = new IMSTim871A();
 metisII.MessageReceived += (sender, message) =>
 {
     // Do something with the message
@@ -19,4 +19,4 @@ metisII.IntializeModule(port, baudRate);
 Task.Run(() => metisII.ListenToMessages(port, baudRate, new CancellationToken()));
 
 Console.WriteLine("Waiting for messages. Press any key to exit.");
-Console.ReadKey();
+Console.Read();
---
+++ src/Yrki.IoT.WMBus.IMSTim871A/IMSTim871A.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Yrki.IoT.WMBus.IMSTim871A
+{
+    public class IMSTim871A
+    {
+        public event EventHandler<byte[]>? MessageReceived;
+
+        /// <summary>
+        /// Setting up the USB stick with preferred parameters. Should be done once for each stick.
+        /// Is setting up the the module to receive mode for C1 and T1-mode.
+        /// Is setting up the the module to include RSSI in the output.
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="baudrate"></param>
+        public void IntializeModule(string port, int baudrate)
+        {
+            var serialPort = new SerialPort(port, baudrate, Parity.None, 8, StopBits.One);
+            serialPort.Open();
+            serialPort.Close();
+        }
+
+        public Task ListenToMessages(string port, int baudrate, CancellationToken cancellationToken)
+        {
+            System.Diagnostics.Debug.WriteLine("Listening to messages");
+            var serialPort = new SerialPort(port, baudrate, Parity.None, 8, StopBits.One);
+            serialPort.Open();
+
+            var charactersLeft = 0;
+            List<byte> message = new List<byte>();
+
+            while(true)
+            {
+                System.Diagnostics.Debug.WriteLine("Here");
+                var value = serialPort.ReadByte();
+                System.Diagnostics.Debug.WriteLine(value.ToString("X2"));
+                // var buffer = new byte[serialPort.BytesToRead];
+                // if(buffer.Length > 1)
+                // {
+                //     serialPort.Read(buffer, 0, buffer.Length);
+                //     Console.WriteLine(BitConverter.ToString(buffer));
+
+                    // for (int i = 0; i < buffer.Length; i++)
+                    // {
+                    //     if(charactersLeft <= 0 && (i < buffer.Length - 1) && buffer[i] == 0xFF && buffer[i+1] == 0x03)
+                    //     {
+                    //         charactersLeft = buffer[i+2];
+
+                    //         if(message.Count > 0)
+                    //         {
+                    //             MessageReceived?.Invoke(this, message.ToArray());
+                    //             message = new List<byte>();
+                    //         }
+                    //     }
+
+                    //     message.Add(buffer[i]);
+                    //     charactersLeft--;
+                    // }
+                // }
+                // Thread.Sleep(100);
+
+                if(cancellationToken.IsCancellationRequested)
+                {
+                    serialPort.Close();
+                    break;
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
